5c5eb2d5beff1b1b64580a844b290b1bad874770
Leandrorocha1983 <lrocha1983@gmail.com>
2023-02-01 09:03:25 -0300

    Upload inicial RestAssured API

---
+++ TesteApi/src/main/java/com/reqres/in/TesteApi/App.java
@@ -0,0 +1,13 @@
+package com.reqres.in.TesteApi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ TesteApi/src/test/java/com/reqres/in/TesteApi/TesteQa.java
@@ -0,0 +1,49 @@
+package com.reqres.in.TesteApi;
+import static io.restassured.RestAssured.given;
+
+import org.hamcrest.core.StringContains;
+import org.junit.Test;
+
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.response.Response;
+
+@SuppressWarnings("unused")
+public class TesteQa {
+
+	/*@Test
+	public void reqresapi() {
+		given().when().get("https://reqres.in/api-docs/#/default/post_login").then().log().all();
+	
+   }
+*/
+
+
+	@Test 
+	public void cadastroApi() {
+		given()
+			.contentType("application/json")
+			.body("{\"username\": \"leandroroha\", \"email\": \"lrocha1983@gmail.com\", \"password\": \"123456\"}")
+		.when()
+			.post("https://reqres.in/api-docs/#/default/post_login")
+		.then()
+			.statusCode(200)
+			.log().all();
+		
+	}
+	
+	
+		@Test 
+		public void cadastroApiCampoObrigatorio() {
+			given()
+				.contentType("application/json")
+				.body("{\"password\": \"123456\"}")
+			.when().post("https://reqres.in/api-docs/#/default/post_login")
+			.then()
+				.log().all()
+				.body("message", ContainsString("\"email\" is required"))
+			   	.statusCode(200);
+		
+		
+	
+		}
+}
